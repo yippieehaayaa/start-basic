6b5b33e12413d3421eae7ca90f3e9c4ca34ec17e
Joshua Dave Oropilla <163279835+yippieehaayaa@users.noreply.github.com>
2026-03-12 22:46:36 +0800

    Add project files

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    netlify(),
+    tailwindcss(),
+    tsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tanstackStart({
+      srcDirectory: 'src',
+    }),
+    viteReact(),
+    nitro(),
+  ],
+})
